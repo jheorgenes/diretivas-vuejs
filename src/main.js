2445463c4f58212999236d9e2cf5ff7c0ab2c777
Jheorgenes Warlley <jheorgenes@gmail.com>
2023-07-21 16:51:19 -0300

    - Aprendendo a utilizar diretivas  - Entendo o funcionamento de diretivas customizadas.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,32 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const Vue = createApp(App);
+
+Vue.directive('texto', {
+
+  //created() é chamado antes que os atributos do elemento ou ouvintes de event(event listeners) sejam aplicados.
+  created(el) {
+    // console.log(el); //Parametro el guarda a referência da diretiva (que nesse caso é uma tag p com um texto dentro)
+    console.log(el.style); 
+    el.style.color = 'red';
+    el.style.fontSize = '150%' // Alteração de estilização em diretivas só utiliza o CamelCase para os atributos do css.
+
+    let textoOriginal = el.innerText;
+    let tamanhoTextoOriginal = textoOriginal.length;
+    let textoAjustado = '';
+
+    if(tamanhoTextoOriginal > 25) {
+      //vamos truncar o texto em 22 caracteres e adicionar '...'
+      textoAjustado = textoOriginal.substring(0, 22) + '...';
+    } else {
+      //vamos manter o texto original
+      textoAjustado = textoOriginal;
+    }
+
+    el.innerText = textoAjustado;
+    console.log('A diretiva foi aplicada com sucesso.');
+  }
+})
+
+Vue.mount('#app')
