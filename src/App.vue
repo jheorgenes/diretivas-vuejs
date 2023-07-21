4b2224b2bd7e7e797666225de4a83714e6bfb4b9
Jheorgenes Warlley <jheorgenes@gmail.com>
2023-07-21 17:17:00 -0300

    - Aprendendo a utilizar o parametro binding das diretivas customizadas

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <p>Sem a diretiva</p>
-  <p v-texto>Minha primeira diretiva customizada</p>
+  <p v-texto="{ cor: 'red', tamanhoFonte: '200%', totalCaracteres: 15 }">Minha primeira diretiva customizada</p><!-- Quando quiser usar uma string, deve-se usar as aspas simples, se não vai entender como variavel -->
+  <p v-texto="{ cor: 'blue', tamanhoFonte: '300%', totalCaracteres: 4 }">Teste</p>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -6,19 +6,22 @@ const Vue = createApp(App);
 Vue.directive('texto', {
 
   //created() é chamado antes que os atributos do elemento ou ouvintes de event(event listeners) sejam aplicados.
-  created(el) {
-    // console.log(el); //Parametro el guarda a referência da diretiva (que nesse caso é uma tag p com um texto dentro)
-    console.log(el.style); 
-    el.style.color = 'red';
-    el.style.fontSize = '150%' // Alteração de estilização em diretivas só utiliza o CamelCase para os atributos do css.
+  created(el, binding) {
+
+    if(binding.value?.cor) el.style.color = binding.value.cor;
+    if(binding.value?.tamanhoFonte) el.style.fontSize = binding.value.tamanhoFonte;
+    
+    let totalCaracteres = 25;
+
+    if(binding.value?.totalCaracteres) totalCaracteres = binding.value.totalCaracteres;
 
     let textoOriginal = el.innerText;
     let tamanhoTextoOriginal = textoOriginal.length;
     let textoAjustado = '';
 
-    if(tamanhoTextoOriginal > 25) {
+    if(tamanhoTextoOriginal > totalCaracteres) {
       //vamos truncar o texto em 22 caracteres e adicionar '...'
-      textoAjustado = textoOriginal.substring(0, 22) + '...';
+      textoAjustado = textoOriginal.substring(0, (totalCaracteres - 3)) + '...';
     } else {
       //vamos manter o texto original
       textoAjustado = textoOriginal;
